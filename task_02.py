8df7eadba22465f01238d88a7f1ca2a67bcbfff9
elenagottardi <elenagottardi.99@gmail.com>
2023-03-06 15:44:31 +0100

    task_02 done transform

--- task_02.py
+++ task_02.py
@@ -2,6 +2,7 @@
 
 # pip install pandas
 import pandas as pd
+from datetime import datetime
 
 # EXTRACT
 # from json file to dataframe
@@ -9,7 +10,25 @@ def json_to_dataframe():
     print(f'Note: from json to dataframe')
     df = pd.read_json('data/orders.json', lines=True)
     df.info()
+    return df
+
+# TRANSFORM
+def fix_column_type(df):
+    print(f'Note: fix column type')
+    # datetime
+    df['ShipDate'] = pd.to_datetime(df['ShipDate'])
+    df['LatestShipDate'] = pd.to_datetime(df['LatestShipDate'])
+    df['PurchaseDate'] = pd.to_datetime(df['PurchaseDate'])
+    # string
+    df['OrderStatus'] = df['OrderStatus'].astype('string')
+    df['OrderType'] = df['OrderType'].astype('string')
+    df['IsReplacementOrder'] = df['IsReplacementOrder'].astype('string')
+    # int
+    df['NumberOfItemsShipped'] = df['NumberOfItemsShipped'].astype('int64')
+    # dict
+    # df['ShippingAddress'] = df['ShippingAddress'].to_list()
+    df.info()
 
 # MAIN
 if __name__ == '__main__':
-    json_to_dataframe()
+    fix_column_type(json_to_dataframe())
